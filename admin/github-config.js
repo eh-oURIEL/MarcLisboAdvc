71e901d44055e05af9f3cc07a533685362a2c400
Uriel <112789972+eh-oURIEL@users.noreply.github.com>
2025-12-22 10:35:05 -0300

    Delete admin directory

--- admin/github-config.js
+++
@@ -1,8 +0,0 @@
-
-// CONFIGURAÇÃO DO GITHUB
-localStorage.setItem("github_user", "eh-oURIEL");
-localStorage.setItem("github_repo", "MarcLisboAdvc");
-localStorage.setItem("github_branch", "main");
-
-// Você precisará definir seu token manualmente aqui:
-localStorage.setItem("github_token", "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");
